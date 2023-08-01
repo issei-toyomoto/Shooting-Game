542830930dc9d45ae8984293bbb18a8fbdfa50fd
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2023-08-01 16:34:51 +0900

    環境構築
    FPS、シーンマネージャー、を追加

---
+++ Common.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include"DxLib.h"
+
+//画面サイズ
+#define _SCREEN_HEIGHT_ 480	//縦
+#define _SCREEN_WIDHT_	640	//横
+
+//色
+#define C_RED   GetColor(255,0,0)		//赤
+#define C_GREEN GetColor(0,255,0)		//緑
+#define C_BLUE  GetColor(0,0,255)		//青
+#define C_BLACK GetColor(0,0,0)			//黒
+#define C_WHITE GetColor(255,255,255)	//白
